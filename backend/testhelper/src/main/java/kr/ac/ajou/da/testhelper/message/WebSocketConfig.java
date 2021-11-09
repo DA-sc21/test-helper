80024875ef7ec7413b449e56152e8cfbe56a20f4
nyw <skdud@ajou.ac.kr>
2021-11-09 22:51:17 +0900

    22-2 config spring websocket

---
+++ backend/testhelper/src/main/java/kr/ac/ajou/da/testhelper/message/WebSocketConfig.java
@@ -0,0 +1,23 @@
+package kr.ac.ajou.da.testhelper.message;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+    	// websocket 연결주소
+        registry.addEndpoint("/chatting").setAllowedOriginPatterns("*").withSockJS();
+    }
+}
