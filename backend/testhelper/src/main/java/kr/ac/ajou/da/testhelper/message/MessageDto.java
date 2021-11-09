50e2408723d1fbcdda2b79cb84e4958b815a263a
nyw <skdud@ajou.ac.kr>
2021-11-09 23:00:20 +0900

    22-2 MessageDto 설정

---
+++ backend/testhelper/src/main/java/kr/ac/ajou/da/testhelper/message/MessageDto.java
@@ -0,0 +1,38 @@
+package kr.ac.ajou.da.testhelper.message;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class MessageDto implements Serializable {
+    private String message;
+    private String user;
+    private Long timeStamp;
+
+    private String fileName;
+    private String rawData;
+    
+    public MessageDto() {
+    }
+
+    public MessageDto(String message, String user) {
+        this.user = user;
+        this.message = message;
+    }
+
+    public MessageDto(String fileName, String rawData, String user) {
+
+        this.fileName = fileName;
+        this.rawData = rawData;
+        this.user = user;
+    }
+
+    public MessageDto(String message) {
+        this.message = message;
+    }
+}
