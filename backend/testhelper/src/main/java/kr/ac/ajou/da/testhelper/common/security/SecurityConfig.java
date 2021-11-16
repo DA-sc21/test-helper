dfeaecbd0c6bbab7ac9a59efeac4542a41d8a38c
jmchoi1225 <jmchoi1234@hotmail.com>
2021-11-16 22:31:20 +0900

    31-2,31-3,37-2 by-pass swagger from Spring Security

--- backend/testhelper/src/main/java/kr/ac/ajou/da/testhelper/common/security/SecurityConfig.java
+++ backend/testhelper/src/main/java/kr/ac/ajou/da/testhelper/common/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,6 +19,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(accountService);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                //swagger
+                .antMatchers("/swagger-ui.html")
+                .antMatchers("/webjars/springfox-swagger-ui/**")
+                .antMatchers("/configuration/ui")
+                .antMatchers("/swagger-resources")
+                .antMatchers("/v2/api-docs");
     }
 
     @Bean
