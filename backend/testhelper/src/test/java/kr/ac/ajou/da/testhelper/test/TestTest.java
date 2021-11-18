40defd74836ae8abb7f71fb5f0674741d676d9b2
jmchoi1225 <jmchoi1234@hotmail.com>
2021-11-19 03:47:24 +0900

    17-2 can't end test before endTime

--- backend/testhelper/src/test/java/kr/ac/ajou/da/testhelper/test/TestTest.java
+++ backend/testhelper/src/test/java/kr/ac/ajou/da/testhelper/test/TestTest.java
@@ -2,9 +2,12 @@ package kr.ac.ajou.da.testhelper.test;
 
 import kr.ac.ajou.da.testhelper.common.dummy.DummyFactory;
 import kr.ac.ajou.da.testhelper.test.definition.TestStatus;
+import kr.ac.ajou.da.testhelper.test.exception.CannotEndTestBeforeEndTimeException;
+import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class TestTest {
 
@@ -19,5 +22,21 @@ class TestTest {
         //then
         assertEquals(TestStatus.ENDED, test.getStatus());
 
+    }
+
+    @Test
+    @Ignore // TODO : LocalDateTime.now()를 mock할 방법 찾기
+    void updateStatus_ENDED_nowBeforeEndTime_throw_CannotEndTestBeforeEndTimeException(){
+        //given
+        kr.ac.ajou.da.testhelper.test.Test test = DummyFactory.createTest();
+
+        //LocalDateTime.now() < test.getEndTime()
+
+        //when
+        assertThrows(CannotEndTestBeforeEndTimeException.class, () -> {
+            test.updateStatus(TestStatus.ENDED);
+        });
+
+        //then
     }
 }
