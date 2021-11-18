3f2e7254139758f94383189c55f884d5938d4e72
jmchoi1225 <jmchoi1234@hotmail.com>
2021-11-19 03:51:26 +0900

    17-2 refactor : move tests -> test

--- backend/testhelper/src/main/java/kr/ac/ajou/da/testhelper/test/TestController.java
+++ backend/testhelper/src/main/java/kr/ac/ajou/da/testhelper/test/TestController.java
@@ -5,15 +5,21 @@ import kr.ac.ajou.da.testhelper.common.dto.BooleanResponse;
 import kr.ac.ajou.da.testhelper.test.dto.PutTestStatusReqDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class TestController {
 
     private final TestService testService;
+
+    @GetMapping("/tests")
+    public List<HashMap<String, Object>> getTests(@RequestParam int accountId) throws Exception {
+        return testService.getTests(accountId);
+    }
 
     @PutMapping("/tests/{testId}/status")
     public ResponseEntity<BooleanResponse> putTestStatus(@PathVariable Long testId,
--- backend/testhelper/src/main/java/kr/ac/ajou/da/testhelper/test/TestService.java
+++ backend/testhelper/src/main/java/kr/ac/ajou/da/testhelper/test/TestService.java
@@ -1,6 +1,7 @@
 package kr.ac.ajou.da.testhelper.test;
 
 import kr.ac.ajou.da.testhelper.account.Account;
+import kr.ac.ajou.da.testhelper.account.AccountMapper;
 import kr.ac.ajou.da.testhelper.test.definition.TestStatus;
 import kr.ac.ajou.da.testhelper.test.exception.TestNotFoundException;
 import kr.ac.ajou.da.testhelper.test.room.TestRoomService;
@@ -8,12 +9,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class TestService {
 
     private final TestRepository testRepository;
     private final TestRoomService testRoomService;
+    private final TestsMapper testsMapper;
+    private final AccountMapper accountMapper;
 
     @Transactional
     public Test getTest(Long testId) {
@@ -32,5 +39,21 @@ public class TestService {
             testRoomService.deleteRoomsForStudents(testId, updatedBy.getId());
         }
 
+    }
+
+    public List<HashMap<String, Object>> getTests(int accountId) throws SQLException {
+        String role = getAccountRole(accountId);
+
+        if(role.equals("PROFESSOR")) {
+            return testsMapper.getTestListOfProfessor(accountId);
+        } else if(role.equals("ASSISTANT")) {
+            return testsMapper.getTestListOfAssistant(accountId);
+        }
+
+        return null;
+    }
+
+    private String getAccountRole(int accountId) throws SQLException {
+        return accountMapper.getAccountRole(accountId);
     }
 }
