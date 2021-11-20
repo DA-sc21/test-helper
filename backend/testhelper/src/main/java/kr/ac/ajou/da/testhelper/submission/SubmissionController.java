db3c11c2f8f1abffedae61648ff7e10b5e7aec21
jmchoi1225 <jmchoi1234@hotmail.com>
2021-11-20 15:13:26 +0900

    31-2,31-3,37-2 grant access to self examinee info

--- backend/testhelper/src/main/java/kr/ac/ajou/da/testhelper/submission/SubmissionController.java
+++ backend/testhelper/src/main/java/kr/ac/ajou/da/testhelper/submission/SubmissionController.java
@@ -3,11 +3,14 @@ package kr.ac.ajou.da.testhelper.submission;
 import kr.ac.ajou.da.testhelper.common.dto.BooleanResponse;
 import kr.ac.ajou.da.testhelper.common.security.authority.IsExaminee;
 import kr.ac.ajou.da.testhelper.common.security.authority.IsProctor;
+import kr.ac.ajou.da.testhelper.common.security.exception.NotAuthorizedException;
+import kr.ac.ajou.da.testhelper.examinee.Examinee;
 import kr.ac.ajou.da.testhelper.submission.definition.SubmissionType;
 import kr.ac.ajou.da.testhelper.submission.dto.GetSubmissionUploadUrlResDto;
 import kr.ac.ajou.da.testhelper.submission.dto.PutSubmissionConsentedReqDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
@@ -31,7 +34,12 @@ public class SubmissionController {
     @IsExaminee
     public ResponseEntity<GetSubmissionUploadUrlResDto> getSubmissionUploadUrl(@PathVariable Long testId,
                                                                                @PathVariable Long studentId,
-                                                                               @PathVariable SubmissionType submissionType) {
+                                                                               @PathVariable SubmissionType submissionType,
+                                                                               @AuthenticationPrincipal Examinee examinee) {
+
+        if(!(examinee.getTest().getId().equals(testId) && examinee.getStudent().getId().equals(studentId))){
+            throw new NotAuthorizedException();
+        }
 
         return ResponseEntity.ok().body(new GetSubmissionUploadUrlResDto(
                 submissionService.getUploadUrlByTestIdAndStudentIdAndSubmissionType(testId, studentId, submissionType)));
