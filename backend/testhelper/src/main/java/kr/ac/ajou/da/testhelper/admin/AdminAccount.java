cfb6e205a35a9c0e1a9730e54b800badb025ceea
jmchoi1225 <jmchoi1234@hotmail.com>
2021-11-22 23:57:47 +0900

    31-2 관리자 로그인 api 구현

---
+++ backend/testhelper/src/main/java/kr/ac/ajou/da/testhelper/admin/AdminAccount.java
@@ -0,0 +1,76 @@
+package kr.ac.ajou.da.testhelper.admin;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Collections;
+
+@NoArgsConstructor
+@Getter
+@Setter(value = AccessLevel.PRIVATE)
+@Entity
+@Table(name = "ADMIN_ACCOUNT")
+public class AdminAccount implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    public AdminAccount(Long id) {
+        this.id = id;
+    }
+
+    public AdminAccount(Long id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_ADMIN"));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
