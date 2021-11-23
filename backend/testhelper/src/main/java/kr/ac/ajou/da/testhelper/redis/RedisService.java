8a2bfa897923666788139455012482c10447432c
nyw <skdud@ajou.ac.kr>
2021-11-23 22:25:03 +0900

    35-2 인증코드 만료 & 잘못된 인증 코드 errormessage 추가

--- backend/testhelper/src/main/java/kr/ac/ajou/da/testhelper/redis/RedisService.java
+++ backend/testhelper/src/main/java/kr/ac/ajou/da/testhelper/redis/RedisService.java
@@ -5,11 +5,17 @@ import java.time.Duration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import kr.ac.ajou.da.testhelper.email.EmailServiceImpl;
 import kr.ac.ajou.da.testhelper.email.dto.PostEmailConfirmReqDto;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
+@Slf4j
 public class RedisService {
 	
 	private final int LIMIT_TIME = 3 * 60; // 3분
@@ -32,7 +38,14 @@ public class RedisService {
 	}
 	
 	public boolean isVerify(PostEmailConfirmReqDto reqDto) {
-		return getRedisValue(reqDto.getEmail()).equals(reqDto.getCode());
+		String value = getRedisValue(reqDto.getEmail());
+		if(value == null) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "인증 코드가 만료되었습니다.");
+		}
+		if(!value.equals(reqDto.getCode())) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "잘못된 인증 코드입니다.");
+		}
+		return value.equals(reqDto.getCode());
 	}
 
 }
