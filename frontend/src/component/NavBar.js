e16f352bb71a92b4ccee3f6269c68214c4ed5dc3
jiyeon <s1990626@ajou.ac.kr>
2021-11-26 19:57:00 +0900

    NavBar 색상 변경

--- frontend/src/component/NavBar.js
+++ frontend/src/component/NavBar.js
@@ -4,9 +4,9 @@ import { Navbar , Nav , NavDropdown , Container  } from 'react-bootstrap';
 
 function NavBar(){
   return(
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="dark" variant="dark" expand="lg" style={{height:"9vh"}}>
       <Container fluid>
-        <Navbar.Brand as = {Link} to="/">Test Helper</Navbar.Brand>
+        <Navbar.Brand as = {Link} to="/" style={{fontSize:"25px"}}>Test Helper</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
@@ -14,9 +14,9 @@ function NavBar(){
           style={{ maxHeight: '100px' }}
           navbarScroll
           >
-            <Nav.Link as = {Link} to="/" > Home </Nav.Link>
-            <Nav.Link as = {Link} to="/courses" > 강의 </Nav.Link>
-            <Nav.Link as = {Link} to="/tests" > 시험목록 </Nav.Link>
+            <Nav.Link as = {Link} to="/" style={{fontSize:"17px", marginTop:"2%"}}> Home </Nav.Link>
+            <Nav.Link as = {Link} to="/courses" style={{fontSize:"17px", marginTop:"3%"}}> 강의 </Nav.Link>
+            <Nav.Link as = {Link} to="/tests" style={{fontSize:"17px", marginTop:"3%"}}> 시험목록 </Nav.Link>
             {/* <Nav.Link as = {Link} to="/tests/1/students/1" >1번대학생시험준비(완료 후 navbar에선 삭제예정)</Nav.Link> */}
             {/* <NavDropdown title="Link" id="navbarScrollingDropdown">
               <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
@@ -29,8 +29,8 @@ function NavBar(){
           </Nav>
         </Navbar.Collapse>
         <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-          Signed in as: <a href="#login">윤대균 교수</a>
+          <Navbar.Text style={{fontSize:"17px", marginTop:"0.5%"}}>
+          Signed in as: <a href="#login" style={{textUnderlinePosition:"under"}}>윤대균 교수</a>
           </Navbar.Text>
         </Navbar.Collapse>
       </Container>
