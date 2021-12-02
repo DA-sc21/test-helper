d43506d00ada18dfb97b02b82619c0a96385edb6
jiyeon <s1990626@ajou.ac.kr>
2021-12-03 03:26:20 +0900

    51-1 답안지 응시자 학번 검색

--- frontend/src/professor/StudentAnswerSheets.js
+++ frontend/src/professor/StudentAnswerSheets.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Card, Badge } from 'react-bootstrap';
+import { Button, Card, Badge, InputGroup, FormControl } from 'react-bootstrap';
 import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 import {baseUrl} from "../component/baseUrl";
@@ -9,6 +9,8 @@ import './StudentAnswerSheets.css';
 function StudentAnswerSheets(props){
   let history = useHistory();
   let path = props.path;
+  const [state, setState] = useState([]);
+  const [allStudents, setAllstudents] = useState([]);
   const [students, setStudents] = useState([]);
   let scoring_status={
     "PENDING" : "미채점",
@@ -23,12 +25,48 @@ function StudentAnswerSheets(props){
 
   useEffect(()=>{
     console.log(props);
+    setAllstudents(props.students);
     setStudents(props.students);
   },[]);
 
+  function onChangehandler(e){
+    let { name , value} = e.target;
+    setState({
+      ...state,
+      [name]: value,
+    });
+    console.log(state);
+  }
+
+  let searchStudentNumber = (e) => {
+    if(state.studentNumber.length>0){
+      const filterList = allStudents.filter((data) => {
+        return data.student.studentNumber.toLowerCase().includes(state.studentNumber);
+      });
+      console.log(filterList);
+      setStudents(filterList);
+    }
+    else{
+      setStudents(allStudents);
+    }
+  }
+
+  function checkAllStudentList(){
+    setStudents(allStudents);
+  }
+
   return(
-    <div style={{marginLeft:"7%", marginTop:"2%", width:"70%"}}>
-      <div style={{marginTop:"3%", marginBottom:"0.2%", width:"90%", fontSize:"19px", textAlign:"left"}}>
+    <div style={{marginLeft:"7%", marginTop:"1%", width:"70%"}}>
+      <Button variant="light" style={{marginRight:"10%", float:"right", color:"black", borderColor:"gray"}} onClick={(e)=>searchStudentNumber(e)}>검색</Button>
+      <InputGroup style={{width:"30%", float:"right"}}>
+        <InputGroup.Text>학번</InputGroup.Text>
+        <FormControl
+          name="studentNumber" 
+          onChange={(e)=>onChangehandler(e)}
+        />
+      </InputGroup>
+      <Button style={{marginRight:"2.5%", float:"right", backgroundColor:"#3d4657", borderColor:"#3d4657"}} onClick={(e)=>checkAllStudentList(e)}>전체 목록</Button>
+      <div style={{marginTop:"5%", marginBottom:"0.2%", width:"90%", fontSize:"19px", textAlign:"left"}}>
         <span style={{marginLeft:"5%"}}>학번</span>
         <span style={{marginLeft:"6%"}}>이름</span>
         <span style={{marginRight:"1.7%", float:"right"}}>채점 여부</span>
