3ea374b6e6ad8cac17ad39fb0935c7ca65cd162a
nyw <skdud@ajou.ac.kr>
2021-12-09 23:27:12 +0900

    33-2 impl. createStudent Mapper

--- backend/testhelper/src/main/java/kr/ac/ajou/da/testhelper/student/StudentMapper.java
+++ backend/testhelper/src/main/java/kr/ac/ajou/da/testhelper/student/StudentMapper.java
@@ -2,6 +2,7 @@ package kr.ac.ajou.da.testhelper.student;
 
 import java.sql.SQLException;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
@@ -12,4 +13,8 @@ import org.springframework.stereotype.Repository;
 public interface StudentMapper {
 	@Select("SELECT student_num FROM STUDENT WHERE id = ${studentId}")
 	String getStudentNum(@Param("studentId") int studentId) throws SQLException;
+
+	@Insert("INSERT INTO STUDENT (name, student_num, email, course_id) VALUES ('${name}', '${studentNum}', '${email}', ${courseId})")
+	void createStudent(@Param("name") String name, @Param("studentNum") String studentNum, @Param("email") String email, @Param("courseId") Long courseId);
+	
 }
---
+++ backend/testhelper/src/main/java/kr/ac/ajou/da/testhelper/student/StudentService.java
@@ -0,0 +1,19 @@
+package kr.ac.ajou.da.testhelper.student;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class StudentService {
+	
+	private final StudentMapper studentMapper;
+	
+	public void createStudent(String name, String studentNum, String email, Long courseId) {
+		studentMapper.createStudent(name, studentNum, email, courseId);
+	}
+
+}
