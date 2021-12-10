e912811bccd1b06e80317a250a975aaaf3860d23
lilyLee7 <ls_hyeon@naver.com>
2021-12-11 04:45:17 +0900

    답안 삭제

--- frontend/src/professor/CreateAnswerExample.js
+++ frontend/src/professor/CreateAnswerExample.js
@@ -6,11 +6,8 @@ import { baseUrl } from '../component/baseUrl';
 import axios from 'axios';
 
 function CreateAnswerExample(props) {
-  const [show, setShow] = useState(false);
   let [img, setImg] = useState("");
   let [answers, setanswers] = useState([]);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
   let { testId } = useParams();
 
   useEffect(()=>{
@@ -36,8 +33,9 @@ function CreateAnswerExample(props) {
 
     let preSignedUrl="";
     let testIdPad=String(testId).padStart(5,"0")
+    let answerIdPad=String(answers.length+1).padStart(3,"0")
   
-    let response = await fetch(baseUrl+'/s3-upload-url?objectKey=test/'+testIdPad+'/answer_sheet/001.jpg',{
+    let response = await fetch(baseUrl+'/s3-upload-url?objectKey=test/'+testIdPad+'/answer_sheet/'+answerIdPad+'.jpg',{
       method: "GET",
       credentials: "include"
     })
@@ -50,7 +48,7 @@ function CreateAnswerExample(props) {
 
      console.log(preSignedUrl);
      const data = {
-      "file": "test/"+testIdPad+"/answer_sheet/001.jpg"
+      "file": "test/"+testIdPad+"/answer_sheet/"+answerIdPad+".jpg"
     };
      let response2 = await fetch(baseUrl+'/tests/'+testId+'/answers',{
       method: 'POST',
@@ -76,6 +74,27 @@ function CreateAnswerExample(props) {
     
   }
   
+  async function deleteAnswer(answerId){
+
+    let response = await fetch(baseUrl+`/tests/`+testId+`/answers/`+answerId,{
+      method: 'DELETE',
+      credentials : 'include',
+    })
+    .then( res => {
+      console.log("response:", res);
+      getTestAnswerSheet();
+      if(res.status === 200){
+        alert("답안이 삭제되었습니다.");
+      }
+      else{
+        alert("답안 삭제에 실패했습니다.");
+      }
+      }
+    )
+    .catch(error => {console.error('Error:', error)});
+
+  }
+
   return (
     <>
       <Form>
@@ -89,32 +108,29 @@ function CreateAnswerExample(props) {
         </Col>
         <Col sm={2}>
         <Button variant="dark" onClick={()=>{
-            let selectedFileName=document.querySelector("#formFile").value
-            selectedFileName=null
-            if (img===""){
-              alert("파일을 선택해주세요")
-            }
-            else{
-              UploadImageToS3(img)
-            }
-            setImg("")
-          }}>답안 등록</Button>
+          let selectedFileName=document.querySelector("#formFile").value
+          selectedFileName=null
+          if (img===""){
+            alert("파일을 선택해주세요")
+          }
+          else{
+            UploadImageToS3(img)
+          }
+          setImg("")
+        }}>답안 등록</Button>
         </Col>
         </Row>
         <Row>
 
-          {answers.map((t,index)=>{
+          {answers.map((answer,index)=>{
           return (
             <div key={index} className="col-md-6">
               <Card className="mb-3" >
-                <Card.Img variant="top" src={t.file} />
-                <Card.Footer>
-                  <Button className="mx-3" variant="success">답안 수정</Button>
-                  <Button  variant="danger">답안 삭제</Button>
-                  {/* <ProblemUpdateModal updateProblems={updateProblems} problemNum={problem.problemNum} point={problem.point} question={problem.question}></ProblemUpdateModal> */}
-                  {/* <Button variant="danger" onClick={()=>{
-                    deleteProblems(problem.problemNum)
-                  }}>문제 삭제</Button> */}
+                <Card.Img variant="top" src={answer.file} />
+                <Card.Footer className="d-flex justify-content-end" >
+                  <Button variant="outline-danger" onClick={()=>{
+                    deleteAnswer(answer.id)
+                  }}>답안 삭제</Button>
                 </Card.Footer>
               </Card>
             </div>
