8b0318790da16cde5e92108be7bbd904966d5461
lilyLee7 <ls_hyeon@naver.com>
2021-12-11 12:52:39 +0900

    교수/조교 답안지 캡쳐본 조회

--- frontend/src/professor/AnswerSheet/ScoringTests.js
+++ frontend/src/professor/AnswerSheet/ScoringTests.js
@@ -11,6 +11,7 @@ function ScoringTests(props){
   const [state, setState] = useState([]);
   const [score, setScore] = useState(0);
   const [imageUrl, setImageUrl] = useState("");
+  const [captureimageUrl, setcaptureImageUrl] = useState("");
   const [submissionId, setSubmissionId] = useState("");
   const [TestAnswerSheetUrl, setTestAnswerSheetUrl] = useState([]);
   const [TestAnswerSheetImgUrl, setTestAnswerSheetImgUrl] = useState("");
@@ -47,13 +48,14 @@ function ScoringTests(props){
 
   async function getAnswerSheet(problem){
     await axios
-    .get(baseUrl+path+`/students/${props.studentId}/submissions?includeCapture=false`,{
+    .get(baseUrl+path+`/students/${props.studentId}/submissions?includeCapture=true`,{
         withCredentials : true
       })
     .then((result)=>{
       console.log(result.data);
       setSubmissionId(result.data.id);
       setImageUrl(result.data.answerSheetDownloadUrl);
+      setcaptureImageUrl(result.data.captureSheetDownloadUrl);
       setLoading(true); 
     })
     .catch(()=>{ console.log("실패") })
@@ -146,19 +148,63 @@ function ScoringTests(props){
 			  }
 		  </ListGroup>
 		</Col>
-		<Col sm={9}>
+		<Col sm={10}>
 		  <Tab.Content>
 			{
 			  problems.map((problem,index)=>{
 				return (
 				  <Tab.Pane key={index} eventKey={"#problem"+index}>
-					  <span style={{fontSize:"17px"}}>
-              {problem.question.split("\n").map((line)=>{
-							  return <div>{line}</div>
-							})}
-            </span>
-            {/* <Image className="col-md-5" src={problem.attachedFile} /> */}
-            <div style={{float:"right", marginRight:"38%", marginTop:"1%", backgroundColor:"#ffb649", width:"100px", textAlign:"center", padding:"2px", borderRadius:"5px", fontWeight:"bold"}}>시험 답안지</div>
+            <div className="row d-flex justify-content-end">
+              <div className="col-md-4">
+                <InputGroup >
+                  <InputGroup.Text>점수</InputGroup.Text>
+                    <FormControl
+                      name="score"
+                      value={state.score}
+                      onChange={(e)=>onChangehandler(e)}
+                    />
+                  <Button style={{backgroundColor:"#575757", borderColor:"gray"}} onClick={(e)=>enterScore(e,index+1)}>입력</Button>
+                </InputGroup>
+                </div>
+              </div>
+              <div className="row my-3">
+                <div className="col-md-12">
+                  <span style={{fontSize:"17px"}}>
+                    {problem.question.split("\n").map((line)=>{
+                      return <div>{line}</div>
+                    })}
+                  </span>
+                  <Image className="col-md-12" src={problem.attachedFile} />
+                <div>
+              </div>
+            </div>
+            </div>
+            <div className="row">
+              <div className="col-md-4">
+                <div style={{backgroundColor:"#ffb649", textAlign:"center", padding:"4px",  margin:"10px", borderRadius:"5px", fontWeight:"bold"}}>시험 답안지</div>
+                <div style={{border:"1px solid gray", height:"70vh", borderRadius:"5px"}}>
+                  <Pagination style={{marginBottom:"0%"}}>
+                    {TestAnswerSheetUrl.map((data,idx)=>(
+                      <Pagination.Item key={idx+1} active={idx+1 === active} onClick={()=>changeTestAnswerSheet(idx)}>
+                      {idx+1}
+                    </Pagination.Item>
+                    ))}
+                  </Pagination>
+                  {TestAnswerSheetImgUrl != ""? 
+                    <Image src={TestAnswerSheetImgUrl} style={{width:"100%", height:"92%", marginTop:"0%"}}/>:
+                    <h4 style={{marginTop:"50%", textAlign:"center"}}>등록된 답안이 없습니다.</h4>}
+                </div>
+              </div>
+              <div className="col-md-4">
+                <div style={{backgroundColor:"#ffc0cb", textAlign:"center", padding:"4px", margin:"10px", borderRadius:"5px", fontWeight:"bold"}}>학생 답안지 제출본</div>
+                <Image src={imageUrl} style={{  borderRadius:"5px", height:"70vh", width:"100%"}}/>
+              </div>
+              <div className="col-md-4">
+                <div style={{backgroundColor:"#59a5fc", textAlign:"center", padding:"4px", margin:"10px", borderRadius:"5px", fontWeight:"bold"}}>학생 답안지 캡쳐본</div>
+                <Image src={captureimageUrl} style={{ borderRadius:"5px", width:"100%", height:"70vh"}}/>
+              </div>
+            </div>
+            {/* <div style={{float:"right", marginRight:"38%", marginTop:"1%", backgroundColor:"#ffb649", width:"100px", textAlign:"center", padding:"2px", borderRadius:"5px", fontWeight:"bold"}}>시험 답안지</div>
             <div style={{marginLeft:"13%", marginTop:"1%", backgroundColor:"#59a5fc", width:"100px", textAlign:"center", padding:"2px", borderRadius:"5px", fontWeight:"bold"}}>학생 답안지</div>
             <div style={{border:"1px solid gray", float:"right", width:"35%", height:"70vh", marginRight:"25%", borderRadius:"5px"}}>
               <Pagination style={{marginBottom:"0%"}}>
@@ -173,6 +219,7 @@ function ScoringTests(props){
                 <h4 style={{marginTop:"50%", textAlign:"center"}}>등록된 답안이 없습니다.</h4>}
             </div>
             <Image src={imageUrl} style={{width:"35%", height:"70vh", marginTop:"0%"}}/>
+            <Image src={imageUrl2} style={{width:"35%", height:"70vh", marginTop:"0%"}}/>
               
             <Button style={{marginTop:"1%", backgroundColor:"#575757", borderColor:"gray"}} onClick={(e)=>enterScore(e,index+1)}>입력</Button>
             <InputGroup style={{float:"left", width:"30%", marginTop:"1%"}}>
@@ -182,7 +229,8 @@ function ScoringTests(props){
                 value={state.score}
                 onChange={(e)=>onChangehandler(e)}
               />
-            </InputGroup>
+            </InputGroup> */}
+
 				  </Tab.Pane>
 				)
 			  })
