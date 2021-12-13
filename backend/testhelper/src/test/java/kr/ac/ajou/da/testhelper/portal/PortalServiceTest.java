5377f45ac180f6f6a693415734a0d1fefc2a448d
nyw <skdud@ajou.ac.kr>
2021-12-13 18:32:58 +0900

    0 add. PortalServiceTest

---
+++ backend/testhelper/src/test/java/kr/ac/ajou/da/testhelper/portal/PortalServiceTest.java
@@ -0,0 +1,57 @@
+package kr.ac.ajou.da.testhelper.portal;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import kr.ac.ajou.da.testhelper.account.definition.AccountRole;
+import kr.ac.ajou.da.testhelper.definition.PortalStatus;
+
+class PortalServiceTest {
+	@InjectMocks
+	private PortalService portalService;
+	@Mock
+	private PortalRepository portalRepository;
+	
+	@BeforeEach
+	void init() {
+		portalRepository = mock(PortalRepository.class);
+		portalService = new PortalService(portalRepository);
+	}
+	
+	@Test
+	void getCourseById_success() {
+		//given
+		Long courseId = 1L;
+		String code = "code";
+		
+		String name = "name";
+		String email = "email";
+		AccountRole role = AccountRole.PROFESSOR;
+		PortalAccount professor = new PortalAccount(name, email, role);
+		
+		List<PortalAssistant> assistants = new ArrayList<>();
+		PortalStatus registered = PortalStatus.DONE;
+		List<PortalStudent> students = new ArrayList<>();
+		
+		PortalCourse expectedCourses = new PortalCourse(courseId, code, name, professor, assistants, registered, students);
+		
+		when(portalRepository.findById(anyLong())).thenReturn(Optional.of(expectedCourses));
+
+		//when
+		PortalCourse actualCourses = portalService.getCourseById(courseId);
+		
+		//then
+		verify(portalRepository, times(1)).findById(anyLong());
+		assertEquals(expectedCourses, actualCourses);
+	}
+}
